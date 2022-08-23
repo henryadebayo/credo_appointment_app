afdd604ec981ae566d26b0fcfaa5a31712888081
henry <henryadebayo250@gmail.com>
2022-08-24 00:20:23 +0100

    Initial commit

---
+++ lib/utils/const/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color appBarAshColor = const Color(0xff424242);
+  static Color appOxbloodColor = const Color(0xff861F41);
+  static Color appGreenColor = const Color(0xff23CB1F);
+  static Color appRedColor = const Color(0xffD53434);
+}
